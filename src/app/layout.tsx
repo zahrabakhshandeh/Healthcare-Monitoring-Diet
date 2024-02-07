6755c7ce802ebe6c1051434b96f57ed40c29fd27
Mohammad Mosavat <42.octmst@gmail.com>
2024-02-07 21:34:06 +0330

    add a style of background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className + " bg-slate-50"}>{children}</body>
     </html>
   );
 }
