0a3420bbfde5f18120cea46fda3f897f4bcd9f4a
MohammadMosavat <42.octmst@gmail.com>
2024-02-08 15:21:36 +0330

    import FormRegister and call it

--- src/app/register/signup/page.tsx
+++ src/app/register/signup/page.tsx
@@ -1,8 +1,6 @@
-import MainInput from "@/components/MainInput";
 import SignUpArt from "/public/signup.svg";
-import MainButton from "@/components/MainButton";
 import Link from "next/link";
-
+import FormRegister from "./formRegister";
 const SignUpPage = () => {
   return (
     <main className="bg-slate-50 flex px-5  justify-around items-center h-screen">
@@ -18,21 +16,7 @@ const SignUpPage = () => {
             خودتون رو به ما معرفی کنید
           </p>
         </div>
-        <form action="" className="flex w-full flex-col gap-4">
-          <MainInput className="w-full" placeholder="کد ملی" />
-          <MainInput className="w-full" placeholder="نام و نام خانوادگی" />
-          <MainInput className="w-full" placeholder="رمز عبور" />
-          <MainInput className="w-full" placeholder="تکرار رمز عبور" />
-          <MainInput className="w-full" placeholder="شماره تماس یا ایمیل" />
-          <section className="flex flex-row-reverse items-center justify-between">
-            <MainButton className="w-52 " value={"ثبت نام"} />
-            <Link href={"/"}>
-              <p className="text-lg text-[var(--primary-blue-color)]">
-                قبلا ثبت نام کردید؟
-              </p>
-            </Link>
-          </section>
-        </form>
+        <FormRegister/>
       </section>
     </main>
   );
