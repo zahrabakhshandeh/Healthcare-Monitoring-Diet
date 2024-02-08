838d65420b232c09379ce59a74719fe4f5809dab
Parsa Bordbar <parsab71@gmail.com>
2024-02-08 20:52:36 +0330

    Updated with Typography tags and sx for styling

--- src/app/register/login/page.tsx
+++ src/app/register/login/page.tsx
@@ -19,31 +19,36 @@ export default function Login() {
   return (
     <section>
       <Grid container spacing={0}>
-        
-        <Grid xs={6} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-          <div className='flex items-center flex-col '>
-            <div className='flex flex-col items-start gap-3'>
+        <Grid xs={6} sx={{padding: '38px'}}>
+          <LoginArt/>
+        </Grid>
+        <Grid xs={6} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+          <div className='flex flex-col items-end text-right'>
+            <div className='flex flex-col items-end gap-3'>
             <Typography sx={{fontSize: '36px', paddingBottom: '92px'}} variant="h3" component="h2">ورود | ثبت نام</Typography>
               <Typography sx={{fontSize: 24}}>نام‌کاربری و رمز عبور خود را وارد کنید</Typography>
 
-              <MainInput className="w-full" placeholder="نام کاربری"/>
+              <MainInput className="w-full " placeholder="نام کاربری"/>
               <MainInput placeholder="رمز عبور"/>
               <div className='flex flex-row gap-10 pt-7 items-center'>
-                <Link href="/" className='text-blue-600'>رمز عبورتون رو فراموش کردید؟</Link>
                 <MainButton className="w-44 font-bold" value={"ثبت نام"}/>
+                <Link href="/" className='text-blue-600'>رمز عبورتون رو فراموش کردید؟</Link>
               </div>
             </div>
           </div>
 
           <div>
-            <Typography></Typography>
+            <Typography sx={{padding: '68px'}}>
+              یا ورود با
+            </Typography>
+
+            <ul>
+            </ul>
+
           </div>
         </Grid>
-            <Grid xs={6} sx={{padding: 2}}>
-              <LoginArt/>
-            </Grid>
+            
       </Grid>
    </section>
   )
 }
-
