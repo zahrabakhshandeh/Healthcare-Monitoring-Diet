6896341c17c76da497d41b34675b156a9433896a
MohammadMosavat <42.octmst@gmail.com>
2024-02-08 15:22:37 +0330

    add props and use callback funtion to handel and check icon exist or not

--- src/components/MainButton/index.tsx
+++ src/components/MainButton/index.tsx
@@ -1,16 +1,24 @@
-import { ButtonHTMLAttributes } from "react";
+"use client";
+import { ButtonHTMLAttributes, ReactNode, useCallback } from "react";
 
 type TButton = ButtonHTMLAttributes<HTMLButtonElement> & {
   value: string;
   className?: string;
+  iconSrc?: any;
 };
 
 const MainButton = (props: TButton) => {
+  const checkIcon = useCallback(() => {
+    if (props.iconSrc) return <props.iconSrc />;
+  }, []);
   return (
     <button
-      className={`${props.className} font-semibold flex items-center justify-center bg-[var(--orange-color)] p-2 rounded-lg text-white`}
+      className={`${props.className} font-semibold flex ${
+        props.iconSrc && "gap-2"
+      } items-center justify-center bg-[var(--orange-color)] p-2 rounded-lg text-white`}
     >
-      {props.value}
+      {checkIcon()}
+      <span>{props.value}</span>
     </button>
   );
 };
