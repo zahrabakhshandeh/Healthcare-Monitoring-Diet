a742570924567665eba284d20d8399ea398f1543
Parsa Bordbar <parsab71@gmail.com>
2024-02-10 01:18:03 +0330

    Add LoginButtons for editinal Loging ways.

--- src/components/LoginOptiontsButton/index.tsx
+++ src/components/LoginOptiontsButton/index.tsx
@@ -3,35 +3,27 @@ import GoogleLogo from "/public/google.svg";
 import FaceBookLogo from "/public/facebook.svg";
 import AppleLogo from "/public/apple.svg";
 
-const LoginByButton = (props: { icon: any  , title:string}) => {
-  const checkIcon = useCallback(() => {
-    if (props.icon) return <props.icon />;
-  }, []);
-  // if(props.variant === 'google') {
-  //     return(
-  //         <button type="submit" className="bg-[var(--new-gray)] p-4 rounded-3xl">
-  //             <GoogleLogo/>
-  //         </button>
-  //     )
-  // }
-  // else if (props.variant === 'apple'){
-  //     <button  className="bg-[var(--new-gray)] p-4 rounded-3xl" type="submit">
-  //         <AppleLogo/>
-  //     </button>
-  // }
-  // else{
-  //     <button  className="bg-[var(--new-gray)] p-4 rounded-3xl" type="submit">
-  //             <FaceBookLogo/>
-  //         </button>
-  // }
-  return (
-    <button
-      title={props.title}
-      className="bg-[var(--new-gray)] shadow-sm drop-shadow-lg p-3 rounded-3xl"
-      type="submit"
-    >
-      {checkIcon()}
-    </button>
-  );
+const LoginByButton = (props: any) => {
+    if(props.variant === 'google') {
+        return(
+            <button type="submit" className="bg-[var(--new-gray)] py-3 px-5 rounded-3xl shadow-md ">
+                <GoogleLogo/>
+            </button>
+        )
+    }
+    else if(props.variant === 'apple'){
+        return(
+        <button  className="bg-[var(--new-gray)] px-5 rounded-3xl shadow-md ">
+            <AppleLogo/>
+        </button>
+    )
+    }
+    else{
+        return(
+            <button className="bg-[var(--new-gray)] px-5 rounded-3xl shadow-md">
+                <FaceBookLogo/>
+            </button>
+            )
+    }
 };
 export default LoginByButton;
