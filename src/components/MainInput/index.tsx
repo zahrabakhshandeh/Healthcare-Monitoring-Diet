8005f614a519372f822e05fd9e4f556c70e5911f
MohammadMosavat <42.octmst@gmail.com>
2024-02-09 20:09:20 +0330

    handle type of inputs

--- src/components/MainInput/index.tsx
+++ src/components/MainInput/index.tsx
@@ -2,54 +2,46 @@
 import { TInput } from "@/types";
 import React from "react";
 import { InputHTMLAttributes, ReactNode, useCallback, useMemo } from "react";
-import OpenEyeICon from '/public/eye.svg'
-import CloseEyeICon from '/public/eye-slash.svg'
+import OpenEyeICon from "/public/eye.svg";
+import CloseEyeICon from "/public/eye-slash.svg";
 
 const MainInput = (props: TInput) => {
-
   const [showPassword, setShowPassword] = React.useState(false);
 
   const checkFirstIcon = useCallback(() => {
-    if (props.firstIconSrc) return <props.firstIconSrc/>;
+    if (props.firstIconSrc) return <props.firstIconSrc />;
   }, []);
 
-  const eyeIconMemo = useMemo(()=>{
-    if (props.type === 'password' && !showPassword){
-      return(
-        <div onClick={() => setShowPassword(!showPassword) }>
+  const eyeIconMemo = useMemo(() => {
+    if (props.type === "password" && !showPassword) {
+      return (
+        <div onClick={() => setShowPassword(!showPassword)}>
           <CloseEyeICon />
         </div>
-      )
-    }
-      else if (showPassword && props.type === 'password'){
-        return(
-          <div onClick={() => setShowPassword(!showPassword) }>
-            <OpenEyeICon/>
-          </div>
-            )
-      }
-      else if (props.type === 'email'){
-          
-      }
-      else{
-        setShowPassword(true);
-      }
-  }, [showPassword])
-
+      );
+    } else if (showPassword && props.type === "password") {
       return (
-        <div
-          className={`flex rounded-2xl items-center shadow-sm drop-shadow bg-[var(--new-gray)] gap-2 p-2 px-4`}
-        >
-          {checkFirstIcon()}
-          <input
-            {...props}
-            className=" outline-none w-full placeholder:text-sm bg-transparent border-none"
-            type={showPassword? 'text' : 'password'}
-            name=""
-            id=""
-          />
-          {eyeIconMemo}
+        <div onClick={() => setShowPassword(!showPassword)}>
+          <OpenEyeICon />
         </div>
       );
+    }
+  }, [showPassword]);
+
+  return (
+    <div
+      className={`flex rounded-2xl items-center shadow-sm drop-shadow bg-[var(--new-gray)] gap-2 p-2 px-4`}
+    >
+      {checkFirstIcon()}
+      <input
+        {...props}
+        className=" outline-none w-full placeholder:text-sm bg-transparent border-none"
+        type={showPassword ? "text" : props.type}
+        name=""
+        id=""
+      />
+      {eyeIconMemo}
+    </div>
+  );
 };
 export default MainInput;
