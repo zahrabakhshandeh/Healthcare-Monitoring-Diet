3189294f8085f222d7baa5ea475e2a700c609cf5
MohammadMosavat <42.octmst@gmail.com>
2024-02-09 20:32:03 +0330

    change the way of render a component and add title for tag that if we hover shows up the name

--- src/components/LoginOptiontsButton/index.tsx
+++ src/components/LoginOptiontsButton/index.tsx
@@ -1,25 +1,37 @@
-import React from "react";
-import GoogleLogo from "/public/google.svg"
-import FaceBookLogo from "/public/facebook.svg"
-import AppleLogo from "/public/apple.svg"
+import React, { ReactNode, useCallback} from "react";
+import GoogleLogo from "/public/google.svg";
+import FaceBookLogo from "/public/facebook.svg";
+import AppleLogo from "/public/apple.svg";
 
-const LoginByButton = (props:any) =>{
-        if(props.variant === 'google') {
-            return(
-                <button type="submit" className="bg-[var(--new-gray)] p-4 rounded-3xl">
-                    <GoogleLogo/>
-                </button>
-            )
-        }
-        else if (props.variant === 'apple'){
-            <button  className="bg-[var(--new-gray)] p-4 rounded-3xl" type="submit">
-                <AppleLogo/>
-            </button>
-        }
-        else{
-            <button  className="bg-[var(--new-gray)] p-4 rounded-3xl" type="submit">
-                    <FaceBookLogo/>
-                </button>
-        }
+const LoginByButton = (props: { icon: any  , title:string}) => {
+  const checkIcon = useCallback(() => {
+    if (props.icon) return <props.icon />;
+  }, []);
+  // if(props.variant === 'google') {
+  //     return(
+  //         <button type="submit" className="bg-[var(--new-gray)] p-4 rounded-3xl">
+  //             <GoogleLogo/>
+  //         </button>
+  //     )
+  // }
+  // else if (props.variant === 'apple'){
+  //     <button  className="bg-[var(--new-gray)] p-4 rounded-3xl" type="submit">
+  //         <AppleLogo/>
+  //     </button>
+  // }
+  // else{
+  //     <button  className="bg-[var(--new-gray)] p-4 rounded-3xl" type="submit">
+  //             <FaceBookLogo/>
+  //         </button>
+  // }
+  return (
+    <button
+      title={props.title}
+      className="bg-[var(--new-gray)] shadow-sm drop-shadow-lg p-3 rounded-3xl"
+      type="submit"
+    >
+      {checkIcon()}
+    </button>
+  );
 };
 export default LoginByButton;
