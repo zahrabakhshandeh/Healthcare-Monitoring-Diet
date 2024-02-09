42f4e87a1ff67a3765e8b86da1c9ca94cc1d2107
MohammadMosavat <42.octmst@gmail.com>
2024-02-09 20:04:18 +0330

    give inputs type and validate other type of inputs

--- src/app/register/signup/formRegister/index.tsx
+++ src/app/register/signup/formRegister/index.tsx
@@ -6,31 +6,44 @@ import MainButton from "@/components/MainButton";
 import PasswordKey from "/public/passwordkey.svg";
 import Email from "/public/email.svg";
 import ID from "/public/id.svg";
-import SignUp from '/public/signup-icon.svg'
+import SignUp from "/public/signup-icon.svg";
 
 const FormRegister = () => {
   return (
     <form action="" className="flex w-full flex-col gap-4">
-      <MainInput className="w-full" firstIconSrc={ID} placeholder="کد ملی" />
+      <MainInput
+        type="text"
+        className="w-full"
+        firstIconSrc={ID}
+        placeholder="کد ملی"
+      />
       <MainInput
         className="w-full"
+        type="text"
         firstIconSrc={UserProfile}
         placeholder="نام و نام خانوادگی"
       />
-      <MainInput type="password"
+      <MainInput
+        type="password"
         className="w-full"
         firstIconSrc={PasswordKey}
         placeholder="رمز عبور"
       />
-      <MainInput type="password"
+      <MainInput
+        type="password"
         className="w-full"
         firstIconSrc={PasswordKey}
         placeholder="تکرار رمز عبور"
       />
-      <MainInput className="w-full" firstIconSrc={Email} placeholder="ایمیل" />
+      <MainInput
+        type="email"
+        className="w-full"
+        firstIconSrc={Email}
+        placeholder="ایمیل"
+      />
       <section className="flex flex-row-reverse items-center justify-between">
         <MainButton className="w-52 " iconSrc={SignUp} value={"ثبت نام"} />
-        <Link href={"/"}>
+        <Link href={"/register/login"}>
           <p className="text-lg text-[var(--primary-blue)]">
             قبلا ثبت نام کردید؟
           </p>
