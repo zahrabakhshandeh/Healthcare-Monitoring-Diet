6827467eca422a3ef1587df0a4126bd9bd9dba54
MohammadMosavat <42.octmst@gmail.com>
2024-02-09 20:47:42 +0330

    create a simple header with list of link for navifate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,15 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex gap-2  p-24">
-      <Link href={"/register/signup"}>ثبت نام</Link>
-      <hr />
-      <Link href={"/register/login"}>ورود</Link>
+    <main className="">
+      <header className="flex w-full p-4 bg-[var(--new-gray)]  items-center gap-2 ">
+        <Link className="hover:text-[var(--primary-blue)]" href={"/register/signup"}>
+          <span>ثبت نام</span>
+        </Link>
+        <Link className="hover:text-[var(--primary-blue)]" href={"/register/login"}>
+          <span>ورود</span>
+        </Link>
+      </header>
     </main>
   );
 }
