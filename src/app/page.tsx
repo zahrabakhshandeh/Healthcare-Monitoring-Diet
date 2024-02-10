9f458c79d3fe761a4e55f87400909967f3e6942f
Parsa Bordbar <parsab71@gmail.com>
2024-02-10 12:38:51 +0330

    Fix Link elements routes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <main className="">
       <header className="flex w-full p-4 bg-[var(--new-gray)]  items-center gap-2 ">
-        <Link className="hover:text-[var(--primary-blue)]" href={"/register/signup"}>
+        <Link className="hover:text-[var(--primary-blue)]" href={"register/signup"}>
           <span>ثبت نام</span>
         </Link>
-        <Link className="hover:text-[var(--primary-blue)]" href={"/register/login"}>
+        <Link className="hover:text-[var(--primary-blue)]" href={"register/login"}>
           <span>ورود</span>
         </Link>
       </header>
