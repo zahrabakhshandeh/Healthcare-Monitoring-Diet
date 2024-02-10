1ec722e670364ed8a169d42b87dded34ff14cc54
MohammadMosavat <42.octmst@gmail.com>
2024-02-10 14:54:55 +0330

    use Fragment tag for parent

--- src/app/register/signup/page.tsx
+++ src/app/register/signup/page.tsx
@@ -5,7 +5,7 @@ import '../../globals.css'
 
 const SignUpPage = () => {
   return (
-    <main className="bg-slate-50 flex px-5  justify-around items-center h-screen">
+    <>
       <div className="bg-slate-50">
         <SignUpArt />
       </div>
@@ -20,7 +20,7 @@ const SignUpPage = () => {
         </div>
         <FormRegister/>
       </section>
-    </main>
+    </>
   );
 };
 
