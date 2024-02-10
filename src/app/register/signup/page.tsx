16407555fa87473e4629aba285883c69e798a4a2
MohammadMosavat <42.octmst@gmail.com>
2024-02-10 19:34:20 +0330

    responsive page

--- src/app/register/signup/page.tsx
+++ src/app/register/signup/page.tsx
@@ -3,13 +3,12 @@ import FormRegister from "./formRegister";
 
 const SignUpPage = () => {
   return (
-    <main className="bg-slate-50 flex px-5  justify-around items-center h-screen">
-      <div className="bg-slate-50">
-        <SignUpArt />
-      </div>
+    <main className="flex min-[500px]:flex-col-reverse px-5  justify-around items-center md:flex-row  md:h-screen  min-[500px]:gap-12 md:gap-0 pt-10">
+      <SignUpArt />
+
       <section className="flex items-start w-96 flex-col gap-8">
         <div className="flex flex-col gap-16 items-start">
-          <h1 className="text-4xl text-[var(--black)] font-extrabold">
+          <h1 className="text-[2.25rem] text-[var(--black)] font-extrabold">
             ثبت نام
           </h1>
           <p className="text-2xl text-[var(--black)] font-semibold">
