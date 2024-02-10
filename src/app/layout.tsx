ce209f3c78571f6b3572a503f7f90c5f71f7f0b2
MohammadMosavat <42.octmst@gmail.com>
2024-02-10 15:11:10 +0330

    remove style from body yag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className + 'text-[var(--black)]'}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
