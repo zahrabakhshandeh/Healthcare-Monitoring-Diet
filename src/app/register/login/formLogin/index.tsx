25bfb35e002a90ed5f18629b38b0dd25d6184b85
MohammadMosavat <42.octmst@gmail.com>
2024-02-10 19:07:11 +0330

    import login icon and import MainButton in this form

--- src/app/register/login/formLogin/index.tsx
+++ src/app/register/login/formLogin/index.tsx
@@ -1,22 +1,37 @@
-'use client'
-import MainInput from "@/components/MainInput"
+"use client";
+import MainInput from "@/components/MainInput";
 import UserProfile from "/public/profile-circle.svg";
 import PasswordKey from "/public/passwordkey.svg";
- 
+import Link from "next/link";
+import MainButton from "@/components/MainButton";
+import LoginIcon from "/public/login-white.svg";
+
 const FormLogin = () => {
-    return(
-        <form action="" className="flex w-full flex-col gap-4">
-                <MainInput
-                  firstIconSrc={UserProfile}
-                  className="w-full"
-                  placeholder="نام کاربری"
-                />
-                <MainInput
-                  firstIconSrc={PasswordKey}
-                  type="password"
-                  placeholder="رمز عبور"
-                />
-              </form>
-    )
-}
+  return (
+    <>
+      <form action="" className="flex w-full flex-col gap-4">
+        <MainInput
+          firstIconSrc={UserProfile}
+          className="w-full"
+          placeholder="نام کاربری"
+        />
+        <MainInput
+          firstIconSrc={PasswordKey}
+          type="password"
+          placeholder="رمز عبور"
+        />
+      <div className="flex flex-row gap-10 pt-7 items-center">
+        <Link href="/" className="text-[var(--primary-blue)]">
+          رمز عبورتون رو فراموش کردید؟
+        </Link>
+        <MainButton
+          iconSrc={LoginIcon}
+          className="w-44 font-bold"
+          value={"ورود"}
+        />
+      </div>
+      </form>
+    </>
+  );
+};
 export default FormLogin;
