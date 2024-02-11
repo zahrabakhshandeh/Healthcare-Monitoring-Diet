bd83255405e2a86758a23e15b3aac63e240e1b41
MohammadMosavat <42.octmst@gmail.com>
2024-02-12 01:13:18 +0330

    pass props as register and comment ref because of overwrite

--- src/components/MainInput/index.tsx
+++ src/components/MainInput/index.tsx
@@ -5,14 +5,17 @@ import { InputHTMLAttributes, ReactNode, useCallback, useMemo } from "react";
 import OpenEyeICon from "/public/eye.svg";
 import CloseEyeICon from "/public/eye-slash.svg";
 
-const MainInput = forwardRef<HTMLInputElement, TInput & { onChange: (event: ChangeEvent<HTMLInputElement>) => void }>((props, ref) => {
+const MainInput = forwardRef<
+  HTMLInputElement,
+  TInput & { onChange?: (event: ChangeEvent<HTMLInputElement>) => void }
+>((props, ref) => {
   const [showPassword, setShowPassword] = React.useState(false);
 
   const checkFirstIcon = useCallback(() => {
     if (props.firstIconSrc) return <props.firstIconSrc />;
   }, []);
 
-  const eyeIconMemo = useMemo(() => {
+  const eyeIconMemo = useCallback(() => {
     if (props.type === "password") {
       return (
         <div onClick={() => setShowPassword(!showPassword)}>
@@ -23,16 +26,19 @@ const MainInput = forwardRef<HTMLInputElement, TInput & { onChange: (event: Chan
   }, [showPassword]);
 
   return (
-    <div className={`flex rounded-2xl items-center shadow-sm drop-shadow bg-[var(--new-gray)] gap-2 p-2 px-4`}>
+    <div
+      className={`flex rounded-2xl items-center shadow-sm drop-shadow bg-[var(--new-gray)] gap-2 p-2 px-4`}
+    >
       {checkFirstIcon()}
       <input
+        {...props.register}
         {...props}
-        ref={ref}
+        // ref={ref}
         className="outline-none w-full placeholder:text-sm bg-transparent border-none"
         type={showPassword ? "text" : props.type}
         onChange={props.onChange}
       />
-      {eyeIconMemo}
+      {eyeIconMemo()}
     </div>
   );
 });
