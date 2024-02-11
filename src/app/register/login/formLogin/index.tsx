26684d429129f7cde8a83a407fb2885225297776
Parsa Bordbar <parsab71@gmail.com>
2024-02-11 23:24:25 +0330

    Validation of Login page is fully done, It now can get the ause from component MainInput and can validate it with Yup Resplver and React-hook-form.

--- src/app/register/login/formLogin/index.tsx
+++ src/app/register/login/formLogin/index.tsx
@@ -7,61 +7,60 @@ import Link from "next/link";
 import MainButton from "@/components/MainButton";
 import LoginIcon from "/public/login-white.svg";
 import {loginSchema} from '../../../../validation/loginValidation/index'
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Controller, useForm } from "react-hook-form";
+
 
 const FormLogin = () => {
-  
-  const usernameRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(loginSchema),
+  });
 
-  useEffect(()=>{
-    usernameRef.current?.focus()
-  },[])
-
-  const handelEnter = (event: React.KeyboardEvent<HTMLFormElement>) =>{
-    if(event.key === 'Enter'){
-      if (document.activeElement === usernameRef.current && passwordRef.current){
-        passwordRef.current.focus();
-        event.preventDefault();
-      } 
-    }
-  }
-
-  const loginValidation = async(event: any) =>{
-    event.preventDefault();
-    let formData = {
-      username: event.target[0].value,
-      password: event.target[1].value
-    }
-    const isValid = await loginSchema.isValid(formData);
-    console.log(formData);
-    console.log(isValid);
-  }
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
 
   return (
     <>
-      <form action="" className="flex w-full flex-col gap-4" onKeyDown={(e: React.KeyboardEvent<HTMLFormElement>)=>handelEnter(e)} onSubmit={loginValidation}>
-        <MainInput
-          ref={usernameRef}
-          firstIconSrc={UserProfile}
-          className="w-full"
-          placeholder="نام کاربری"
+      <form action="" className="flex w-full flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          control={control}
+          name="username"
+          render={({ field }) => (
+            <MainInput
+              firstIconSrc={UserProfile}
+              className="w-full"
+              placeholder="نام کاربری"
+              {...field}
+            />
+          )}
         />
-        <MainInput
-          ref={passwordRef}
-          firstIconSrc={PasswordKey}
-          type="password"
-          placeholder="رمز عبور"
+        {errors.username && <p className='text-[var(--red)]'>{errors.username.message}</p>}
+
+        <Controller
+          control={control}
+          name="password"
+          render={({ field }) => (
+            <MainInput
+              firstIconSrc={PasswordKey}
+              type="password"
+              placeholder="رمز عبور"
+              {...field}
+            />
+          )}
         />
-      <div className="flex flex-row gap-10 pt-7 items-center">
-        <Link href="/" className="text-[var(--primary-blue)]">
-          رمز عبورتون رو فراموش کردید؟
-        </Link>
-        <MainButton
-          iconSrc={LoginIcon}
-          className="w-44 font-bold"
-          value={"ورود"}
-        />
-      </div>
+        {errors.password && <p className='text-[var(--red)]'>{errors.password.message}</p>}
+
+        <div className="flex flex-row gap-10 pt-7 items-center">
+          <Link href="/" className="text-[var(--primary-blue)]">
+            رمز عبورتون رو فراموش کردید؟
+          </Link>
+          <MainButton
+            iconSrc={LoginIcon}
+            className="w-44 font-bold"
+            value={"ورود"}
+          />
+        </div>
       </form>
     </>
   );
--- src/components/MainInput/index.tsx
+++ src/components/MainInput/index.tsx
@@ -1,12 +1,11 @@
 "use client";
 import { TInput } from "@/types";
-import React, { forwardRef } from "react";
+import React, { ChangeEvent, forwardRef } from "react";
 import { InputHTMLAttributes, ReactNode, useCallback, useMemo } from "react";
 import OpenEyeICon from "/public/eye.svg";
 import CloseEyeICon from "/public/eye-slash.svg";
 
-//forward Ref is function that get both props and ref and make them into one component, basicly it just pass the ref to the input element.  
-const MainInput = forwardRef((props: TInput, ref:any) => {
+const MainInput = forwardRef<HTMLInputElement, TInput & { onChange: (event: ChangeEvent<HTMLInputElement>) => void }>((props, ref) => {
   const [showPassword, setShowPassword] = React.useState(false);
 
   const checkFirstIcon = useCallback(() => {
@@ -22,7 +21,7 @@ const MainInput = forwardRef((props: TInput, ref:any) => {
       );
     }
   }, [showPassword]);
-  
+
   return (
     <div className={`flex rounded-2xl items-center shadow-sm drop-shadow bg-[var(--new-gray)] gap-2 p-2 px-4`}>
       {checkFirstIcon()}
@@ -31,10 +30,10 @@ const MainInput = forwardRef((props: TInput, ref:any) => {
         ref={ref}
         className="outline-none w-full placeholder:text-sm bg-transparent border-none"
         type={showPassword ? "text" : props.type}
+        onChange={props.onChange}
       />
       {eyeIconMemo}
     </div>
   );
 });
-
 export default MainInput;
