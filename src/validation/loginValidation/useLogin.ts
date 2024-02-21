a492c7b0cb6633ee07f791b0eedcfbed7e26e861
MohammadMosavat <42.octmst@gmail.com>
2024-02-21 09:13:28 +0330

    export useLogin and work on validation of login form

--- src/validation/loginValidation/useLogin.ts
+++ src/validation/loginValidation/useLogin.ts
@@ -1,22 +1,61 @@
-import * as yup from 'yup';
-import { toast } from 'react-toastify';
+import * as yup from "yup";
+// import { toast } from 'react-toastify';
 
+import { LoginDataType } from "@/types";
+import { useCallback } from "react";
+import { toast } from "react-toastify";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useRouter } from "next/router";
 
 export const loginSchema = yup.object({
-    username: yup
+  username: yup
     .string()
-    .min(6, 'نام کاربری باید حداقل ۴ حرف داشته باشد')
-    .max(16 , 'نام کاربری بیشتر از ۱۶ حرف نمی‌تواند باشد')
-    .matches(/^[a-zA-Z][a-zA-Z0-9]/, "نام کاربری باید با حروف شروع شود و شامل حروف و اعداد می‌شود")
-    .required('نام کاربری الزامی است'),
+    .min(6, "نام کاربری باید حداقل ۴ حرف داشته باشد")
+    .max(16, "نام کاربری بیشتر از ۱۶ حرف نمی‌تواند باشد")
+    .matches(
+      /^[a-zA-Z][a-zA-Z0-9]/,
+      "نام کاربری باید با حروف شروع شود و شامل حروف و اعداد می‌شود"
+    )
+    .required("نام کاربری الزامی است"),
 
-    password: yup
+  password: yup
     .string()
-    .min(8,'')
-    .max(16, '')
-    .matches(/[a-z]/, 'رمز عبور باید حداقل یک حرف کوچک داشته باشد')
-    .matches(/[A-Z]/, 'رمز عبور باید حداقل یک حرف بزرگ داشته باشد')
-    .matches(/[0-9]/, 'رمز عبور باید حداقل یک عدد داشته باشد')
-    .matches(/[!@#%^&*(),.?":{}|<>]/, 'رمز عبور باید حداقل یک کاراکتر خاص داشته باشد') 
-    .required('رمز عبور را وارد کنید'),
-})
+    .min(8, "")
+    .max(16, "")
+    .matches(/[a-z]/, "رمز عبور باید حداقل یک حرف کوچک داشته باشد")
+    .matches(/[A-Z]/, "رمز عبور باید حداقل یک حرف بزرگ داشته باشد")
+    .matches(/[0-9]/, "رمز عبور باید حداقل یک عدد داشته باشد")
+    .matches(
+      /[!@#%^&*(),.?":{}|<>]/,
+      "رمز عبور باید حداقل یک کاراکتر خاص داشته باشد"
+    )
+    .required("رمز عبور را وارد کنید"),
+});
+
+const useLogin = () => {
+  const {
+    control,
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<LoginDataType>({
+    resolver: yupResolver(loginSchema),
+  });
+
+  const { push } = useRouter();
+
+  const handelValueInputs = useCallback((data: LoginDataType) => {
+    console.log(data);
+  }, []);
+
+  return {
+    control,
+    handelValueInputs,
+    register,
+    errors,
+    handleSubmit,
+  };
+};
+
+export default useLogin;
