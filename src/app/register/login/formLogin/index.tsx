208a17e2a90b35e846a063cfbc552e5a2b2d8b0d
MohammadMosavat <42.octmst@gmail.com>
2024-02-21 09:07:13 +0330

    add useLogin and get all together code that reletive to validation

--- src/app/register/login/formLogin/index.tsx
+++ src/app/register/login/formLogin/index.tsx
@@ -6,53 +6,49 @@ import PasswordKey from "/public/passwordkey.svg";
 import Link from "next/link";
 import MainButton from "@/components/MainButton";
 import LoginIcon from "/public/login-white.svg";
-import {loginSchema} from '../../../../validation/loginValidation/useLogin'
-import { yupResolver } from "@hookform/resolvers/yup";
 import { Controller, useForm } from "react-hook-form";
-
+import useLogin from "@/validation/loginValidation/useLogin";
 
 const FormLogin = () => {
-
-  const { control, handleSubmit, formState: { errors } } = useForm({
-    resolver: yupResolver(loginSchema),
-  });
-
-  const onSubmit = (data: obejct) => {
-    console.log(data);
-  };
+  const { control, handelValueInputs, handleSubmit } = useLogin();
 
   const usernameRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
 
-  useEffect(()=>{
-    usernameRef.current?.focus()
-  },[])
+  useEffect(() => {
+    usernameRef.current?.focus();
+  }, []);
 
-  const inputRefs = [usernameRef, passwordRef,];
+  const inputRefs = [usernameRef, passwordRef];
 
   const handleEnter = (event: React.KeyboardEvent<HTMLFormElement>) => {
-    if(event.key === 'Enter'){
-      for(let i = 0; i < inputRefs.length; i++){
-        if (document.activeElement === inputRefs[i].current){
-          if(inputRefs[i+1]?.current){
-            inputRefs[i+1].current?.focus();
+    if (event.key === "Enter") {
+      for (let i = 0; i < inputRefs.length; i++) {
+        if (document.activeElement === inputRefs[i].current) {
+          if (inputRefs[i + 1]?.current) {
+            inputRefs[i + 1].current?.focus();
             event.preventDefault();
-          } 
+          }
           break;
-        } 
+        }
       }
     }
-  }
-  
+  };
+
   return (
     <>
-      <form action="" className="flex w-full flex-col gap-4" onSubmit={handleSubmit(onSubmit)} onKeyDown={handleEnter}>
+      <form
+        action=""
+        className="flex w-full flex-col gap-4"
+        onSubmit={handleSubmit(handelValueInputs)}
+        onKeyDown={handleEnter}
+      >
         <Controller
           control={control}
           name="username"
           render={({ field }) => (
             <MainInput
-              firstIconSrc={UserProfile}
+              firsticonsrc={UserProfile}
               className="w-full"
               placeholder="نام کاربری"
               {...field}
@@ -60,14 +56,13 @@ const FormLogin = () => {
             />
           )}
         />
-        {errors.username && <p className='text-[var(--red)]'>{errors.username.message}</p>}
 
         <Controller
           control={control}
           name="password"
           render={({ field }) => (
             <MainInput
-              firstIconSrc={PasswordKey}
+              firsticonsrc={PasswordKey}
               type="password"
               placeholder="رمز عبور"
               {...field}
@@ -75,10 +70,9 @@ const FormLogin = () => {
             />
           )}
         />
-        {errors.password && <p className='text-[var(--red)]'>{errors.password.message}</p>}
 
         <div className="flex flex-row gap-10 pt-7 items-center">
-          <Link href="/password-recovery" className="text-[var(--primary-blue)]">
+          <Link href="/" className="text-[var(--primary-blue)]">
             رمز عبورتون رو فراموش کردید؟
           </Link>
           <MainButton
