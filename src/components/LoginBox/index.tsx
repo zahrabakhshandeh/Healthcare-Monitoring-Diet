788269b731e50295d8135ddfd16799e01d0978dc
ParsaBordbar <parsab71@gmail.com>
2024-02-21 23:25:47 +0330

    Add LoginBox component.

---
+++ src/components/LoginBox/index.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Link from "next/link";
+
+const LoginBox = () => {
+    return(
+        <section>
+            <h3>ورود | ثبت نام</h3>
+            <div></div>
+            <FormLogin />               
+        </section>
+    )
+};
+export default LoginBox;
