a3ba37985ef8ff7c8801d689a84fbdb5fa8ab468
MohammadMosavat <42.octmst@gmail.com>
2024-02-28 08:58:54 +0330

    set a width style to the signup box for the size of under than lg to be full

--- src/components/SignupComponents/SignupBox/index.tsx
+++ src/components/SignupComponents/SignupBox/index.tsx
@@ -5,7 +5,7 @@ import styles from "./styles.module.css";
 const SignupBox = () => {
   return (
     <div
-      className={`${styles.SignupBox} rounded-[3.4rem] bg-white left-0 lg:z-10 relative lg:absolute md:w-[667px]  py-10 px-12 w-full lg:right-auto lg:bottom-auto lg:left-[5%] lg:top-[5%] gap-4 flex flex-col bottom-0 right-0 top-0 scale-75`}
+      className={`${styles.SignupBox}  rounded-[3.4rem] bg-white left-0 lg:z-10 relative lg:absolute lg:w-[667px]  py-10 px-12 w-full lg:right-auto lg:bottom-auto lg:left-[5%] lg:top-[5%] gap-4 flex flex-col bottom-0 right-0 top-0 scale-75`}
     >
       <h2 className="text-2xl font-semibold">ثبت نام </h2>
 
