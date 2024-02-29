bdde0565cd5a815722daeb5998893ab5bdd9e6ae
MohammadMosavat <42.octmst@gmail.com>
2024-02-29 06:21:05 +0330

    add style to button of this form to have their own shadow color

--- src/components/LoginComponents/LoginBox/index.tsx
+++ src/components/LoginComponents/LoginBox/index.tsx
@@ -6,7 +6,7 @@ import styles from "./styles.module.css";
 const LoginBox = () => {
   return (
     <div
-      className={`${styles.SignupBox}  rounded-[3.4rem] bg-white left-0 lg:z-10 relative lg:absolute lg:w-[667px]  py-10 px-12 w-full lg:left-auto lg:bottom-auto lg:right-[5%] lg:top-[5%] gap-4 flex flex-col bottom-0 right-0 top-0 scale-75`}
+      className={`${styles.SignupBox}  rounded-[3.4rem] bg-white left-0 z-20 relative lg:absolute lg:w-[667px]  py-10 px-12 w-full lg:left-auto lg:bottom-auto lg:right-[5%] lg:top-[5%] gap-4 flex flex-col bottom-0 right-0 top-0 scale-75`}
     >
       <h2 className="text-2xl font-semibold">ورود | ثبت نام</h2>
       <div
