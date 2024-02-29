721703306b29b039a7d7fdbe0053d97ec0a94aa1
ParsaBordbar <parsab71@gmail.com>
2024-02-29 10:03:08 +0330

    Add new Components. Add QuickLinkBox component. Add Interface for handling the types of QuickLinkbox.

---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+import {FunctionComponent,} from "react";
+
+export default interface QuickLinkBoxTypes {
+    subject: string, 
+    bodyText:string, 
+    badgeLogo:FunctionComponent|any,
+    badgeColor: String,
+}
