49fdca4bf2bc2ed33342ef2dc9423034c6baa7a8
ParsaBordbar <parsab71@gmail.com>
2024-02-29 19:43:17 +0330

    Finish MessageBox Component. Add MessageBoxArt svg. Add MessageBox styles. Add Responsiveness for  MessageBox.

--- src/components/userPanelComponents/MessageBox/index.tsx
+++ src/components/userPanelComponents/MessageBox/index.tsx
@@ -1,17 +1,24 @@
 import Link from "next/link";
 import Message from "../Message";
+import MessageArt from "/public/svg/messageBoxArt.svg";
 
 const MessageBox = () => {
     return(
-        <div className="bg-[var(--back-ground-white)] flex flex-col gap-2 shadow-md w-full p-10 pb-2 rounded-2xl justify-center ">
-            <div></div>
-            <ul className="flex flex-col gap-2">
+        <div className="bg-[var(--back-ground-white)] flex flex-col gap-2 shadow-md w-full mt-24 pt-24 ms-4 p-8 pb-2 rounded-2xl justify-center relative z-0 ">
+
+            <div className="z-10 absolute -top-2 -right-4 flex items-center justify-center gap-4">
+                <div className="rounded-full bg-[var(--green-btn)] w-24 h-24 flex justify-center items-center  shadow-lg">
+                    <MessageArt />
+                </div>  
+                <p className="text-4xl" >پیام‌ها</p>
+            </div>
+            <ul className="flex flex-col gap-2 ">
                 <li><Message text="پیغام" type=""/></li>
                 <li><Message text="مشکل" type="error"/></li>
                 <li><Message text="هشدار" type="warning"/> </li>
                 <li><Message text="تایید" type="accept"/></li>
             </ul>
-            <Link href="./" className="text-[var(--link-blue)] flex justify-center mt-7">
+            <Link href="./" className="text-[var(--link-blue)] flex justify-center mt-10 mb-2">
                 دیدن بقیه
             </Link>
         </div>
--- src/components/userPanelComponents/QuickLinkBox/index.tsx
+++ src/components/userPanelComponents/QuickLinkBox/index.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 const QuickLinkBox = ({subject, bodyText, badgeLogo, badgeColor}:QuickLinkBoxTypes) => {
     return(
         <Link href="./">
-            <div className="flex justify-center flex-col bg-[var(--box-gray)] 2xl:ps-11 pe-3 pt-10 pb-5 rounded-2xl relative z-0 shadow-md">
+            <div className="flex justify-center flex-col bg-[var(--box-gray)] 2xl:ps-11 pe-3 pt-10 pb-5 rounded-2xl relative z-0 shadow-md mt-24">
                 <div className="flex flex-col gap-1 ">
                     <h1 className="text-xl ps-2">{subject}</h1>
                     <p className="text-sm px-2">{bodyText}</p>
                 </div>
-                <div className={`bottom-2/3 left-3/4 rounded-full flex justify-center items-center w-16 h-16 absolute bg-[var(--primary-blue)] z-10`}>
+                <div className={`bottom-2/3 left-3/4 rounded-full flex justify-center items-center w-16 h-16  bg-[var(--primary-blue)] z-10 absolute`}>
                     <div>
                         {badgeLogo}
                     </div>
