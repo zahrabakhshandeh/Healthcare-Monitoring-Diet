dab6c1a9a9f36d7ef858606b1a0f9514af5a1c3c
ParsaBordbar <parsab71@gmail.com>
2024-03-03 13:03:39 +0330

    Add a route for profilepicture.

--- src/components/userPanelComponents/Profile/index.tsx
+++ src/components/userPanelComponents/Profile/index.tsx
@@ -1,17 +1,21 @@
 import { ProfileType } from "@/types";
 import Image from 'next/image'
 import DefaultAvatar from "/public/svg/default_avatar.svg"
+import Link from "next/link";
 
 const Profile = ({avatar, userName}: ProfileType) => {
     return(
         <div className="flex justify-center items-center flex-col gap-5 scale-75 xl:scale-100">
             <div className="flex justify-start items-center rounded-full bg-white border-[var(--orange-color)] border-2">
-                {avatar ? <Image
-                    src={avatar}
-                    width={180}
-                    height={180}
-                    alt=""
-                />:<DefaultAvatar />}
+                {/* NOTE: Add route later should be dynamic */}
+                <Link href={'/change-avatar'}>
+                    {avatar ? <Image
+                        src={avatar}
+                        width={180}
+                        height={180}
+                        alt=""
+                    />:<DefaultAvatar />}
+                </Link>
             </div>
             <h1 className="cursor-pointer text-white lg:text-3xl xl:text-4xl md:text-lg">{userName}</h1>
         </div>
