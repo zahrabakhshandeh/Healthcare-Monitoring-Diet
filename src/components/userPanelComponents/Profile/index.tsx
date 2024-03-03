d4211b238e6618bb49497afb5ece0f2b8413f3d7
ParsaBordbar <parsab71@gmail.com>
2024-03-03 13:00:10 +0330

    Fix profile avatar section. Add default avatar svg component.

--- src/components/userPanelComponents/Profile/index.tsx
+++ src/components/userPanelComponents/Profile/index.tsx
@@ -1,18 +1,19 @@
 import { ProfileType } from "@/types";
 import Image from 'next/image'
+import DefaultAvatar from "/public/svg/default_avatar.svg"
 
 const Profile = ({avatar, userName}: ProfileType) => {
     return(
         <div className="flex justify-center items-center flex-col gap-5 scale-75 xl:scale-100">
             <div className="flex justify-start items-center rounded-full bg-white border-[var(--orange-color)] border-2">
-                <Image
-                    src={`${avatar}?${avatar}:/public/ProfilePicture/Group 118.png`}
+                {avatar ? <Image
+                    src={avatar}
                     width={180}
                     height={180}
                     alt=""
-                />
+                />:<DefaultAvatar />}
             </div>
-            <h1 className="text-white lg:text-3xl xl:text-4xl md:text-lg">{userName}</h1>
+            <h1 className="cursor-pointer text-white lg:text-3xl xl:text-4xl md:text-lg">{userName}</h1>
         </div>
     )
 };
