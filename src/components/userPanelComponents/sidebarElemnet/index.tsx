a465ef21cd7803c5d3efc2092200a357b74b163a
ParsaBordbar <parsab71@gmail.com>
2024-03-04 00:13:41 +0330

    Add some styles to make BergurMenu Component more responsive.

--- src/components/userPanelComponents/sidebarElemnet/index.tsx
+++ src/components/userPanelComponents/sidebarElemnet/index.tsx
@@ -5,7 +5,7 @@ const SidebarElement = ({text, logo, link}: sideBarElementType) => {
     return(
         <Link
           href={`${link}`}
-          className="cursor-pointer group hover:bg-white w-full py-4 flex items-center ps-6 rounded-s-3xl hover:ease-in-out hover:duration-150 gap-2"
+          className="cursor-pointer group hover:bg-white w-full py-4 flex items-center ps-6 lg:rounded-s-3xl rounded-3xl  hover:ease-in-out hover:duration-150 gap-2"
         >
           {logo}
           <p className="group-hover:text-slate-900 text-white text-xl md:text-4xl lg:text-xl">{text}</p>
